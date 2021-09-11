c8630af54809d5ca9138b65b254e6d76d3e64723
Asus <abgaryanonik2@gmail.com>
2021-09-11 18:32:48 +0400

    else-if

--- src/class2.java
+++ src/class2.java
@@ -1,14 +1,25 @@
 public class class2 {
     public static void main(String[] args) {
-        int month=9;
-        switch (month){
-            case 1:
-            case 2:
-            case 12:
-                System.out.println("It is winter");
-                break;
-            default:
-                System.out.println("It is not winter");
+//        int month=9;
+//        switch (month){
+//            case 1:
+//            case 2:
+//            case 12:
+//                System.out.println("It is winter");
+//                break;
+//            default:
+//                System.out.println("It is not winter");
+//        }
+        boolean redIsHitFromBehind=true;
+        boolean blackIsHitFromBehind=false;
+        if(redIsHitFromBehind && !blackIsHitFromBehind){
+            System.out.println("black");
+        }
+        else if(!redIsHitFromBehind && blackIsHitFromBehind){
+            System.out.println("red");
+        }
+        else{
+            System.out.println("warning");
         }
     }
 }
